e4cdbf64f79fec6f9dc330bd68a66deb1319da2e
Fart Butt <butts@fartcannon.com>
2020-05-25 15:07:50 -0400

    reformat to avoid circular import

--- cogs/botconfig.py
+++ cogs/botconfig.py
@@ -1,7 +1,6 @@
 import logging
 from discord.ext.commands import Bot, Cog, Context, command, has_permissions
-from shared import shitpost
-from shared import guild_configs
+import shared
 
 log = logging.getLogger('bot.' + __name__)
 
@@ -18,25 +17,25 @@ class BotConfig(Cog):
                  (str(ctx.message.author.name), str(ctx.message.channel.name), str(*args)))
         arguments = args[0]
         if arguments == "allow":
-            guild_configs[ctx.message.guild.id].add_channel_to_allowed_channel_list(ctx.message.channel.id)
+            shared.guild_configs[ctx.message.guild.id].add_channel_to_allowed_channel_list(ctx.message.channel.id)
             await ctx.send(
-                shitpost.do_butting_raw_sentence(
+                shared.shitpost.do_butting_raw_sentence(
                     "Buttbot will now talk in this wonderful channel and respond to any message"))
 
         if arguments == "remove":
             await ctx.send(
-                shitpost.do_butting_raw_sentence("Buttbot will longer reply to messages in this channel"))
+                shared.shitpost.do_butting_raw_sentence("Buttbot will longer reply to messages in this channel"))
             self.config.remove_channel_from_allowed_channel_list(
                 ctx.message.channel.id)
 
         if arguments == "botallow":
             await ctx.send(
-                shitpost.do_butting_raw_sentence("I will now reply to the bot on this guild"))
+                shared.shitpost.do_butting_raw_sentence("I will now reply to the bot on this guild"))
             self.config.add_whitelisted_bots(args[1])
 
         if arguments == "botremove":
             await ctx.send(
-                shitpost.do_butting_raw_sentence("I will no longer reply to the bot on this guild"))
+                shared.shitpost.do_butting_raw_sentence("I will no longer reply to the bot on this guild"))
             self.config.remove_whitelisted_bots(args[1])
 
     @config.error
