dabfc43cd75d330d98167650e4eceeb2a35d366d
Fart Butt <butts@fartcannon.com>
2020-05-25 19:10:24 -0400

    discord Check support for valid users

--- butt_library.py
+++ butt_library.py
@@ -1,5 +1,6 @@
-import glob
 import re
+from discord.ext.commands import Context, check
+import shared
 
 from rfc3987 import parse
 
@@ -39,9 +40,44 @@ def detect_code_block(text):
     return False
 
 
-def load_all_config_files():
-    return glob.glob("config/*.ini")
-
-
 def get_indexes(list_, word):
     return [i for (y, i) in zip(list_, range(len(list_))) if word == y]
+
+
+def _should_i_reply_to_bot(ctx: Context):
+    """Checks to see if we should reply to message author.  specific to users discord flags as bots"""
+    if ctx.message.author in shared.guild_configs[ctx.message.guild.id].allowed_bots:
+        # we should always talk to this bot
+        return True
+    else:
+        return False
+
+
+def _should_i_reply_to_user(ctx: Context):
+    """Checks to see if we should reply to message author.  specific to non bot users"""
+    if ctx.message.author not in shared.guild_configs[ctx.message.guild.id].banned_users:
+        return True
+    else:
+        return False
+
+
+def should_i_reply_to_user(ctx: Context):
+    """master clearinghouse for checking if buttbot should reply to user. checks user block list and accepted bot
+    list on a per-guild basis.  also checks global ban list."""
+    if ctx.message.author.bot:
+        # bot user (flag set by discord server)
+        if _should_i_reply_to_bot(ctx):
+            return True
+        else:
+            return False
+    if _should_i_reply_to_user(ctx):
+        return True
+    else:
+        return False
+
+
+def valid_user_or_bot():
+    def predicate(ctx: Context):
+        return should_i_reply_to_user(ctx)
+
+    return check(predicate)
