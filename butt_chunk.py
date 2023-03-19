aaa27e5c66a9097a5b1c3817096766f0024abef2
Fart-Butt <butts@butts.com>
2023-03-18 20:46:27 -0400

    prioritize possessive prepositions when tagging

--- butt_chunk.py
+++ butt_chunk.py
@@ -41,6 +41,7 @@ class ButtChunk:
         if self.usable_chunk:
             self.normalized_tags = self.normalize_tags(self.tag)
             self.get_weights(self.normalized_tags, self.lemma, self.text)
+            self.weight = self.check_posessive_phrases(self.weight)
 
     def build_chunk_word_list(self, chunk, fw=False):
         if len(chunk.text.split(" ")) > 1:
@@ -224,6 +225,12 @@ class ButtChunk:
             "PhraseWeights - updating phrase database completed.  Updated: {} Ignored: {}"
             .format(count_update, count_ignored)
         )
+
+    def _check_posessive_phrases(self, weight: int) -> int:
+        if self.previous_word_tag == "PRP$":
+            return self.weight * 10
+        else:
+            return weight
 
     def __repr__(self):
         return f"""
