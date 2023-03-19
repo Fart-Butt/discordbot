5ea3860182b1b116b4673e9d734c188845298bad
Fart-Butt <36031808+Fart-Butt@users.noreply.github.com>
2023-03-19 17:44:12 -0400

    temporarily remove butted messages from commands

--- cogs/botconfig.py
+++ cogs/botconfig.py
@@ -21,30 +21,25 @@ class BotConfig(Cog):
     @has_permissions(administrator=True)
     async def allow(self, ctx: Context):
         shared.guild_configs[ctx.message.guild.id].add_channel_to_allowed_channel_list(ctx.message.channel.id)
-        await ctx.send(
-            shared.shitpost.do_butting_text(
-                "Buttbot will now talk in this wonderful channel and respond to any message"))
+        await ctx.send("Buttbot will now talk in this wonderful channel and respond to any message")
 
     @config.command()
     @has_permissions(administrator=True)
     async def remove(self, ctx: Context):
-        await ctx.send(
-            shared.shitpost.do_butting_text("Buttbot will longer reply to messages in this channel"))
+        await ctx.send("Buttbot will longer reply to messages in this channel")
         shared.guild_configs[ctx.message.guild.id].remove_channel_from_allowed_channel_list(
             ctx.message.channel.id)
 
     @config.command()
     @has_permissions(administrator=True)
     async def botallow(self, ctx: Context, *args):
-        await ctx.send(
-            shared.shitpost.do_butting_text("I will now reply to the bot on this guild"))
+        await ctx.send("I will now reply to the bot on this guild")
         shared.guild_configs[ctx.message.guild.id].add_whitelisted_bots(args[0])
 
     @config.command()
     @has_permissions(administrator=True)
     async def botremove(self, ctx: Context, *args):
-        await ctx.send(
-            shared.shitpost.do_butting_text("I will no longer reply to the bot on this guild"))
+        await ctx.send("I will no longer reply to the bot on this guild")
         shared.guild_configs[ctx.message.guild.id].remove_whitelisted_bots(args[0])
 
     @config.command()
